e250034d88a10a3795887d4110e39b150e76f1f0
xzboss <942875315@qq.com>
2025-10-07 00:04:21 +0800

    feat: icon

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { cn, colorUtils } from "@/utils/tools";
 import { useAppStore, colorSchemePresets, ColorSchemeKey } from "@/store/app";
+import Icon from "@/components/Icon";
 
 export interface ButtonProps
   extends Omit<React.HTMLAttributes<HTMLDivElement>, "onClick"> {
@@ -60,26 +61,11 @@ const BaseButton = React.forwardRef<HTMLDivElement, ButtonProps>(
         {...props}
       >
         {loading && (
-          <svg
-            className="animate-spin -ml-1 mr-2 h-4 w-4"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            />
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
+          <Icon 
+            type="icon-loading" 
+            className="animate-spin" 
+            style={{ fontSize: "16px" }}
+          />
         )}
         {children}
       </div>
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -5,7 +5,7 @@ import { createFromIconfontCN } from '@ant-design/icons'
  * 基于 @ant-design/icons 的 createFromIconfontCN 创建
  */
 const Icon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/c/font_5034614_m17jkfx1o3r.js',
+  scriptUrl: '//at.alicdn.com/t/c/font_5034614_kya1jhq56tl.js',
 })
 
 export default Icon
